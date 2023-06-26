617a1505664580916c7785515ca713a01e4434c1
Oleksii Nikolaiev <o.nikolaiev@yahoo.com>
2023-06-26 18:38:30 +0300

    Start project

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,47 @@
+import { Component } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+
+import { Overlay, ModalWrap } from 'components/Modal/Modal.styled';
+
+const modalRoot = document.querySelector('#modal-root');
+
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeydown);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeydown);
+  }
+
+  handleKeydown = evt => {
+    if (evt.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleOverlayClick = evt => {
+    if (evt.currentTarget === evt.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <Overlay onClick={this.handleOverlayClick}>
+        <ModalWrap>{this.props.children}</ModalWrap>
+      </Overlay>,
+      modalRoot
+    );
+  }
+}
+
+Modal.propTypes = {
+  element: PropTypes.shape({
+    largeImageURL: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+  }).isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default Modal;
